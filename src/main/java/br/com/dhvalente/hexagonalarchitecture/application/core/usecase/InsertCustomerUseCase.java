3093469121bfb9a1281816344c71314d67231569
Diogo Valente <diogovalente@hotmail.com.br>
2023-11-19 18:07:33 -0300

    Criando a estrutura do projeto

---
+++ src/main/java/br/com/dhvalente/hexagonalarchitecture/application/core/usecase/InsertCustomerUseCase.java
@@ -0,0 +1,29 @@
+/*package br.com.dhvalente.hexagonalarchitecture.application.core.usecase;
+
+public class InsertCustomerUseCase implements InsertCustomerInputPort {
+
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+
+    private final InsertCustomerOutputPort insertCustomerOutputPort;
+
+    private final SendCpfForValidationOutputPort sendCpfForValidationOutputPort;
+
+    public InsertCustomerUseCase(
+            FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
+            InsertCustomerOutputPort insertCustomerOutputPort,
+            SendCpfForValidationOutputPort sendCpfForValidationOutputPort
+    ) {
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.insertCustomerOutputPort = insertCustomerOutputPort;
+        this.sendCpfForValidationOutputPort = sendCpfForValidationOutputPort;
+    }
+
+    @Override
+    public void insert(Customer customer, String zipCode) {
+        var address = findAddressByZipCodeOutputPort.find(zipCode);
+        customer.setAddress(address);
+        insertCustomerOutputPort.insert(customer);
+        sendCpfForValidationOutputPort.send(customer.getCpf());
+    }
+
+}*/
